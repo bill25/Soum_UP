db4d765daaa6c60a99280e483ba23e403e65af61
bill <m-bill5@hotmail.fr>
2017-02-09 12:12:35 +0100

    modified and added new file

---
+++ app/src/androidTest/java/com/example/ge/soum_up/test.java
@@ -0,0 +1,8 @@
+package com.example.ge.soum_up;
+
+/**
+ * Created by GE on 09/02/2017.
+ */
+
+public class test {
+}
--- app/src/main/java/com/example/ge/soum_up/Login.java
+++ app/src/main/java/com/example/ge/soum_up/Login.java
@@ -339,7 +339,10 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
                 mPasswordView.requestFocus();
             }
         }
+public void upload ()
+{
 
+}
         @Override
         protected void onCancelled() {
             mAuthTask = null;
